71be20de9eed9c9111f590872f4cfd0f0f4d0047
mizydorczyk <74381129+mizydorczyk@users.noreply.github.com>
2022-01-28 18:58:30 +0100

    Update

---
+++ scripts.js
@@ -0,0 +1,38 @@
+function compassTemplate(id)
+{
+    const width = 600;
+    var canvas = document.getElementById(id);
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        ctx.fillStyle = '#E84C3D';   //czerwony
+        ctx.fillRect(0, 0, width/2, width/2);
+
+        ctx.fillStyle = '#3598DB';   //niebieski
+        ctx.fillRect(width/2, 0, width/2, width/2);
+
+        ctx.fillStyle = '#2DCC70';   //zielony
+        ctx.fillRect(0, width/2, width/2, width/2);
+
+        ctx.fillStyle = '#8D44AD';   //fiolet
+        ctx.fillRect(width/2, width/2, width/2, width/2);
+
+        ctx.lineWidth = 2;
+
+        ctx.beginPath();
+        ctx.moveTo(width/2, width);
+        ctx.lineTo(width/2, 0);
+        ctx.stroke();
+
+
+        ctx.beginPath();
+        ctx.moveTo(0, width/2);
+        ctx.lineTo(width, width/2);
+        ctx.stroke();
+    }
+}
+
+window.onload = function () {
+    compassTemplate('left-compass');
+    compassTemplate('right-compass');
+};
