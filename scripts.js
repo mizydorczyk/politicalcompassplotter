53c5272dd8a321d54e2f985b5ec253ca751041df
mizydorczyk <74381129+mizydorczyk@users.noreply.github.com>
2022-02-02 18:48:19 +0100

    Still upgrading

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 const width = 600;
 
 function compassTemplate(id) {
-    var canvas = document.getElementById(id);
+    let canvas = document.getElementById(id);
     if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
+        let ctx = canvas.getContext('2d');
 
         ctx.fillStyle = '#E84C3D'; // red
         ctx.fillRect(0, 0, width / 2, width / 2);
@@ -33,11 +33,11 @@ function compassTemplate(id) {
 }
 
 function circle(id, x, y, color, radius) {
-    var canvas = document.getElementById(id);
+    let canvas = document.getElementById(id);
     if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
+        let ctx = canvas.getContext('2d');
         ctx.beginPath();
-        ctx.arc(x, y, radius, 0, 100 * Math.PI);
+        ctx.arc(x, y, radius, 0, 10 * Math.PI);
         ctx.strokeStyle = "black";
         ctx.fillStyle = color;
         ctx.stroke();
@@ -45,64 +45,61 @@ function circle(id, x, y, color, radius) {
     }
 }
 
-function delay() {
-    return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            resolve(42);
-        }, 250);
-    });
-}
-
-async function getData(fileurl) {
-    var array = [];
-    $(document).ready(() => {
-        $.ajax({
-            url: fileurl,
-            dataType: "text",
-            success: function (data) {
-                var row = data.split(/\r?\n|\r/);
-                for (var i = 0; i < data.length; i++) {
-                    if (row[i] != undefined) {
-                        var cell = row[i].split(";");
-                        if (cell != "") {
-                            array.push(cell);
+function getData(fileurl) {
+    p = new Promise(function (resolve, reject) {
+        let array = [];
+        $(document).ready(() => {
+            $.ajax({
+                url: fileurl,
+                dataType: "text",
+                success: function (data) {
+                    let row = data.split(/\r?\n|\r/);
+                    for (let i = 0; i < data.length; i++) {
+                        if (row[i] != undefined) {
+                            var cell = row[i].split(";");
+                            if (cell != "") {
+                                array.push(cell);
+                            }
                         }
                     }
+                    console.log(array);
+                    if (array != undefined) {
+                        resolve(array);
+                    } else {
+                        reject("error")
+                    }
                 }
-            }
+            })
         })
     })
-    await delay();
-    return await array;
+    return p
 }
 
-async function plotData(id, array) {
-    var sumx = 0, sumy = 0;
-    var x, y;
-    console.log(array[0]);
-    for (var i = 0; i < array.length; i++) {
+function plotData(id, array) {
+    let sumx = 0,
+        sumy = 0;
+    let x, y;
+    for (let i = 0; i < array.length; i++) {
         x = array[i][0].replace(",", ".");
         y = array[i][1].replace(",", ".");
-        
+
         x = (parseFloat(x) + 1.0) * width / 2;
         y = (-parseFloat(y) + 1.0) * width / 2;
-        console.log(x, y);
+
         sumx += x;
         sumy += y;
         circle(id, x, y, "red", 11);
     }
-    circle(id, sumx/array.length, sumy/array.length, "blue", 11);
+    circle(id, sumx / array.length, sumy / array.length, "blue", 11);
 }
 
-window.onload = function () {
-    var data1, data2
+window.onload = async function () {
+    let data1, data2;
     compassTemplate('left-compass');
     compassTemplate('right-compass');
 
-    (async function () {
-        data1 = await getData("https://raw.githubusercontent.com/mizydorczyk/plc/main/dane1.csv"); // dane1 2021
-        data2 = await getData("https://raw.githubusercontent.com/mizydorczyk/plc/main/dane2.csv"); // dane2 2022
-        await plotData('left-compass', data1);
-        await plotData('right-compass', data2);
-    })();
+    data1 = await getData("https://raw.githubusercontent.com/mizydorczyk/plc/main/dane1.csv"); // dane1 2021
+    data2 = await getData("https://raw.githubusercontent.com/mizydorczyk/plc/main/dane2.csv"); // dane2 2022
+    plotData('left-compass', data1);
+    plotData('right-compass', data2);
 };
